a819730b878915ae3e0c1b0a3d44c6e335372a83
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-11-29 12:47:57 +0200

    Update problema6.py

--- problema6.py
+++ problema6.py
@@ -1,3 +1,4 @@
+**Se citeste un cuvant si o litera. Sa se afiseze cuvantul alcatuit prin inlocuirea fiecarui caracter al cuvantului cu litera data.**
 c=input("introdu cuvantul: ")
 l=input("introdu litera: ")
 for i in range(len(c)):
