2c7247eb6e3cdc13179736690ba1ab0a48728817
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-11-29 12:41:09 +0200

    Update problema3.py

--- problema3.py
+++ problema3.py
@@ -1,3 +1,6 @@
+**de la tastatura se citesc patru cuvinte, fiecare cuvant fiind citit intr-o singura variabila.Elaborati un program care va forma o fraza ,va
+include toate cuvintele in sir. Fiecare cuvant va fi despartit prin spatiu, ultimul caracter din fraza va fi semnul punct.La ecran se vor afisa
+cuvintele citite cat si fraza formata.**
 a=input("introdu primul cuvant:")
 b=input("introdu al doilea cuvant:")
 c=input("introdu al treilea cuvant:")
