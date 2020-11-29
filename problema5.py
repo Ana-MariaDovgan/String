1c2eb2b11a64a12aa944acf004eab52f467f73f7
Dovgan Ana-Maria <70962178+Ana-MariaDovgan@users.noreply.github.com>
2020-11-29 12:42:26 +0200

    Update problema5.py

--- problema5.py
+++ problema5.py
@@ -1,3 +1,5 @@
+**Se citeste un sir de caractere care reprezinta CNP-ul unei persoane. Sa se verifice corectitudinea lui:numarul de caractere sa fie 13 si toate
+caracterele sa fie cifre.**
 cnp=input("introdu CNP: ")
 nr=0
 if (len(cnp)<13)or(len(cnp)>13):
